4d126298862b8319088fafb74c307d20c7832676
Elisha Bazibuhe <elishabazibuhe7@gmail.com>
2026-01-19 11:02:51 +0000

    ça passer toujours pas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,11 @@
 // You can get this information from the Firebase console.
 
 const firebaseConfig = {
-  "projectId": "studio-466280383-fc285",
-  "appId": "1:352870012434:web:d423c031c76808a3ae6acb",
-  "apiKey": "AIzaSyA0W5Dd4N0d64VGDDS8hJDkI0RZIX0bpe8",
-  "authDomain": "studio-466280383-fc285.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "352870012434"
+  projectId: "studio-466280383-fc285",
+  appId: "1:352870012434:web:d423c031c76808a3ae6acb",
+  apiKey: "AIzaSyA0W5Dd4N0d64VGDDS8hJDkI0RZIX0bpe8",
+  authDomain: "studio-466280383-fc285.firebaseapp.com",
+  messagingSenderId: "352870012434"
 };
 
 export default firebaseConfig;
