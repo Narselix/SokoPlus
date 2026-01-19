0ba96e014923f4c97cc2ad2dd1d797cea773557b
Elisha Bazibuhe <elishabazibuhe7@gmail.com>
2026-01-19 10:53:57 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,14 @@
-// Ce fichier est destiné à être rempli avec la configuration de votre projet Firebase.
-// Vous pouvez obtenir ces informations depuis la console Firebase.
+// This file is intended to be filled with your Firebase project configuration.
+// You can get this information from the Firebase console.
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+  "projectId": "studio-466280383-fc285",
+  "appId": "1:352870012434:web:d423c031c76808a3ae6acb",
+  "apiKey": "AIzaSyA0W5Dd4N0d64VGDDS8hJDkI0RZIX0bpe8",
+  "authDomain": "studio-466280383-fc285.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "352870012434",
+  "storageBucket": "studio-466280383-fc285.appspot.com"
 };
 
 export default firebaseConfig;
